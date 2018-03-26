906ce37b2e7bebfe338f901ff84ce7a66366a0eb
chaoui mohammed <chaoui.90@hotmail.es>
2018-03-26 12:15:53 +0100

    CM: adding first controller

---
+++ meteoAPI/meteoAPI/Controllers/RootController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace meteoAPI.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var Response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+            return Ok(Response);
+        }
+
+    }
+}
